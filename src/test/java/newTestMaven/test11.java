1c106e50cbe714551418602d57df5f88821be209
INFOR\pbalaraju <parimala.balaraju@infor.com>
2019-01-14 12:11:44 +0530

    first maven code in git

---
+++ src/test/java/newTestMaven/test11.java
@@ -0,0 +1,14 @@
+package newTestMaven;
+
+import org.testng.annotations.Test;
+
+public class test11 {
+	
+	
+	@Test
+	public void sampleMethod()
+	{
+		System.out.println("welcome to maven world");
+	}
+
+}
